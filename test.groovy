0b8053f1d1e62012c8bf6d5211d38400c67614b5
sun-long-chao <784966405@qq.com>
2019-12-05 14:38:47 +0800

    修改test.groovy代码

--- test.groovy
+++ test.groovy
@@ -12,7 +12,7 @@ pipeline{
 			steps{
 				script{
 					println "欢迎来到Anthony的Pipeline教程。"
-					model_test = load env.WORKSPACE + "/Pipeline-learn-anthony/pipeline/src/test.groovy"
+					model_test = load env.WORKSPACE + "/Pipeline-learn-anthony/pipeline/test.groovy"
 					println env.WORKSPACE ;
 				}
 			}
