0051e062f5e10be7f1aaed5758e38413c7a0cf07
sun-long-chao <784966405@qq.com>
2019-12-05 14:12:31 +0800

    https://github.com/sun-long-chao/pipeline.git

---
+++ src/pipeline/test.groovy
@@ -0,0 +1,28 @@
+import jenkins.model.*
+ 
+println env.JOB_NAME
+println env.BUILD_NUMBER
+println env.WORKSPACE
+ 
+pipeline{
+	
+	agent any
+	stages{
+		stage("init") {
+			steps{
+				script{
+					println "欢迎来到Anthony的Pipeline教程。"
+					model_test = load env.WORKSPACE + "/Pipeline-learn-anthony/pipeline/src/test.groovy"
+					println env.WORKSPACE ;
+				}
+			}
+		}
+		stage("Test Method") {
+			steps{
+				script{
+					log_files = model_test.find_files('**/*.log')
+				}
+			}
+		}
+	}
+}
