3bdf2fb0479843e5cfdc8ddd3e8dc33acf094575
sun-long-chao <784966405@qq.com>
2019-12-05 17:54:32 +0800

    update

--- module/pipeline-demo-module.groovy
+++ module/pipeline-demo-module.groovy
@@ -1,11 +1,25 @@
 import hudson.model.*;
- 
+
+//查到文件
 def find_files(filetype) {
-	
 	def files = findFiles(glob:filetype)
 	for (file in files) {
 		println file.name
 	}
 }
+//通过文件名称读取JSON格式数据
+def read_json_file(file_path) {
+	def propMap = readJSON file : file_path
+	propMap.each {
+	    println ( it.key + " = " + it.value )
+	}
+}
+//参数为json格式数据
+def read_json_file2(json_string) {
+	def propMap = readJSON text : json_string
+	propMap.each {
+		println ( it.key + " = " + it.value )
+	}
  
 return this;
+}
