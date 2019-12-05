39852035094ef0c180ee1d85317c4379eda720c3
sun-long-chao <784966405@qq.com>
2019-12-05 17:58:39 +0800

    update

---
+++ test_pipeline_readJson.groovy
@@ -0,0 +1,29 @@
+import hudson.model.*;
+ 
+println env.JOB_NAME
+println env.BUILD_NUMBER
+ 
+pipeline{
+	
+	agent any
+	stages{
+		stage("init") {
+			steps{
+				script{
+					model_test = load env.WORKSPACE + "/module/pipeline-demo-module.groovy"
+				}
+			}
+		}
+		stage("Parse json") {
+			steps{
+				script{
+					json_file = env.WORKSPACE + "/testdata/test_json.json"
+					model_test.read_json_file(json_file)
+					println "================================"
+					json_string = '{"NAME":"Anthony","AGE":18,"CITY":"Beijing","GENDER":"male"}'
+					model_test.read_json_file2(json_string)
+				}
+			}
+		}
+	}
+}
