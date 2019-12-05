0a2976ffe8ebf14296f824ed9a2655d6fe257eab
sun-long-chao <784966405@qq.com>
2019-12-05 14:40:32 +0800

    修改输出内容

--- test.groovy
+++ test.groovy
@@ -11,7 +11,7 @@ pipeline{
 		stage("init") {
 			steps{
 				script{
-					println "欢迎来到Anthony的Pipeline教程。"
+					println "欢迎来到sunlongchao的Pipeline教程。"
 					model_test = load env.WORKSPACE + "/Pipeline-learn-anthony/pipeline/test.groovy"
 					println env.WORKSPACE ;
 				}
