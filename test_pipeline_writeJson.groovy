8e7f6a381c2d001861b1636c813f20b37e3b57be
sun-long-chao <784966405@qq.com>
2019-12-06 14:23:50 +0800

    修改JSON数据格式

--- test_pipeline_writeJson.groovy
+++ test_pipeline_writeJson.groovy
@@ -21,7 +21,12 @@ pipeline{
 					tojson_file = env.WORKSPACE + "/testdata/new_json.json"
 					model_test.write_json_to_file(json_file,tojson_file)
 					println "================================"
-					json_string = '{"NAME":"Anthony","AGE":"18","CITY":"Beijing","GENDER":"male"}'
+					json_string = """
+					{"NAME":"Anthony",
+					"AGE":"18",
+					"CITY":"Beijing",
+					"GENDER":"male"
+					}"""
 					tojson_file = env.WORKSPACE + "/testdata/new_json1.json"
 					model_test.write_json_to_file(json_string,tojson_file)
 				}
