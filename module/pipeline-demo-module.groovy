c8b7059eca4edd1004443e7a05980a3e6e908863
sun-long-chao <784966405@qq.com>
2019-12-06 17:21:53 +0800

    unit

--- module/pipeline-demo-module.groovy
+++ module/pipeline-demo-module.groovy
@@ -32,13 +32,11 @@ def write_json_to_file(input_json, tofile_path) {
 		input1 = readJSON file : input_json
 	}else {
 	
-        def jsonSlurper = new JsonSlurper()
-        println "===(￣▽￣) (￣▽￣)  (￣▽￣)   ==333"
+        //def jsonSlurper = new JsonSlurper()
         //获取到的是Map对象
-        def map = jsonSlurper.parseText(input_json)
- 		println "===(￣▽￣) (￣▽￣)  (￣▽￣)   ==444"
-		input1 = map
-		println map 
+        //def map = jsonSlurper.parseText(input_json)
+		input1 = input_json
+		println input1
 	}
 	writeJSON file: tofile_path, json: input1
 }
