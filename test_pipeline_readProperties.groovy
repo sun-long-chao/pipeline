b7ef53db33cc7349bbdfb7af6ddbdf938aed32ed
sun-long-chao <784966405@qq.com>
2019-12-09 11:13:22 +0800

    修改properties文件路径

--- test_pipeline_readProperties.groovy
+++ test_pipeline_readProperties.groovy
@@ -9,14 +9,14 @@ pipeline{
     	stage("init"){
     		steps{
     			script{
-    			    model_test = env.WORKSPACE + "/pipeline/module/pipeline-demo-module.groovy";
+    			    model_test = env.WORKSPACE + "/module/pipeline-demo-module.groovy";
     			}
 			}
 		}
 		stage("read properties"){
 		    steps{
 		       script{
-		       		properties_file = env.WORKSPACE+ "/pipeline/testDate/test.properties";
+		       		properties_file = env.WORKSPACE+ "/testDate/test.properties";
 		       		model_test.read_properties(properties_file);
 		       		println("========================");
 		       }
